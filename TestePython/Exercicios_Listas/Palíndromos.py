74c0761bfb360d0b9a80a47a4e6775d29943b489
Elvis de Arimatea <elvis.arimatea@outlook.com>
2021-01-26 21:09:26 -0300

    Palíndromos_v1
    Tratei a string

--- TestePython/Exercicios_Listas/Palíndromos.py
+++ TestePython/Exercicios_Listas/Palíndromos.py
@@ -1,1 +1,12 @@
 frase = str(input("Digite uma frase: \n\n"))
+
+frase = frase.lower().replace(" ", "") #Trasforma a string "frase" em caixa baixa e remove os espaços
+print(frase)
+
+
+
+
+
+
+
+print("\n\n")
